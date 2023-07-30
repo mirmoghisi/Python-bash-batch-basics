94b98b96567744f52f800f55f560b1c8f0993cd0
Arash Mirmoghisi <133714029+mirmoghisi@users.noreply.github.com>
2023-07-30 01:32:25 +0000

    update

--- Python/simple_exercises_for_control_structures_in_Python.py
+++ Python/simple_exercises_for_control_structures_in_Python.py
@@ -3,8 +3,44 @@
 heights = [1.82,1.75,1.68,1.76,1.5]
 
 # 0. Use for loop to print values from "heights" list 
+print ("0. Use for loop to print values from 'heights' list")
 for i in range(len(heights)):
     print (heights[i])
     
 # 1. Use for loop for printing only values bigger than 1.75
+print ("1. Use for loop for printing only values bigger than 1.75")
+for i in range(len(heights)):
+    if heights[i] > 1.75:
+        print (heights[i])
+
+# 2. Use for loop for printing all values until you find value 1.68 (included)
+print ("2. Use for loop for printing all values until you find value 1.68 (included)")
+for i in range(len(heights)):
+    print (heights[i])
+    if heights[i] == 1.68:
+        break
+
+# 3. Modify the previous loop to print all values from `"heights"` without value 1.68
+print ("3. Modify the previous loop to print all values from 'heights' without value 1.68")
+for i in range(len(heights)):
+    if heights[i] == 1.68:
+        continue
+    print (heights[i])
+
+# 4. use for loop over to iterate over `"heights"` and print "high" for values bigger than 1.75, "medium" for values between 1.68 and 1.75 and "small" for values lower than 1.68
+print ("4. use for loop over to iterate over 'heights' and print 'high' for values bigger than 1.75, 'medium' for values between 1.68 and 1.75 and 'small' for values lower than 1.68") 
+for i in range(len(heights)):
+    if heights[i] > 1.75:
+        print ("high")
+    elif heights[i] < 1.68:
+        print ("small")
+    else:
+        print ("medium")
+        
+# While Loop
+# 1. Print out numbers from 0 to 4
+print ("1. Print out numbers from 0 to 4")
+while i < 5:
+    print (i)
+    i += 1
 
