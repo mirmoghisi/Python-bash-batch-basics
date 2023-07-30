094708e77ad9ae207e1bf57b4fbd372230552fa3
Arash Mirmoghisi <133714029+mirmoghisi@users.noreply.github.com>
2023-07-30 01:18:16 +0000

    update

---
+++ Python/simple_exercises_for_control_structures_in_Python.py
@@ -0,0 +1,10 @@
+"""Simple exercises for control structures in Python."""
+
+heights = [1.82,1.75,1.68,1.76,1.5]
+
+# 0. Use for loop to print values from "heights" list 
+for i in range(len(heights)):
+    print (heights[i])
+    
+# 1. Use for loop for printing only values bigger than 1.75
+
